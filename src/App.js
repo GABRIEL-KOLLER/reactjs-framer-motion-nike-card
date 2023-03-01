6aec559a16671b328955ab58fa0edf22eeb7cc05
Gabriel S. Koller <95451205+GABRIEL-KOLLER@users.noreply.github.com>
2023-02-28 21:25:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './components/Card';
+
+
+const App = () => {
+  return (<div className="w-full bg-zinc-400 h-screen flex items-center justify-center">
+    <Card />
+  </div>
+  );
+};
+
+export default App;
